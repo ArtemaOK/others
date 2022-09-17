c86564fc6193f44e335d4306aa03e41f5f58dd31
Alex <1320538+ArtemaOK@users.noreply.github.com>
2022-09-17 02:22:21 -0300

    modified:   iterator&replacer.js

--- iterator&replacer.js
+++ iterator&replacer.js
@@ -1,5 +1,5 @@
 // Programa para iterar y reemplazar un valor hasta llegar al valor indicado en la línea 4. Colocar en la variable "x" el string a iterar y reemplazar. Se creará un único string en la variable "y" con todas las iteraciones. Ejemplo con iteraciones sobre fórmula de Excel para abarcar las celdas indicadas en 31 hojas.
-let x = '=Hoja1!A1+Hoja1!B1+';
+let x = '+Hoja1!A1+Hoja1!B1';
 let y = '';
 let count = 1;
 while (count < 31) {
@@ -7,4 +7,4 @@ while (count < 31) {
     count += 1;
     y = y + x;
 };
-y.slice(0, -1);
+y;
