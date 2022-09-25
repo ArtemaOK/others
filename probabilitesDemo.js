52d5d9deaa9714046a1aade2b695dd3ba0180efe
Alex <1320538+ArtemaOK@users.noreply.github.com>
2022-09-25 02:23:19 -0300

    new file:   probabilitesDemo.js

---
+++ probabilitesDemo.js
@@ -0,0 +1,21 @@
+// Programa de demostración de probabilidad de que un hecho ocurra (POSITIVE) o no (NEGATIVE) en una cierta cantidad de pruebas.
+let count = 0;
+let number = 0;
+let list = [];
+let object = { POSITIVE: 0, NEGATIVE: 0 };
+// El número de la línea debajo indica la cantidad de pruebas a realizar. 
+while (count < 1000) {
+    number = Math.floor(Math.random() * 100) + 1;
+    // El número de la línea debajo indica la probabilidad teórica (expresada entre 1 y 100) de que un hecho NO ocurra. Si ocurre, resultará POSITIVE.
+    if (number > 99) {
+        list.push('POSITIVE');
+    } else {
+        list.push('NEGATIVE');
+    }
+    count += 1;
+}
+for (const value of list) {
+    object[value] = object[value] += 1;
+}
+// La siguiente línea muestra la cantidad de positivos y negativos que surgen de la prueba.
+console.log(object)
