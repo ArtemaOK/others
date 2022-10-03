11d7e96e18fce1b51f2725699d2a8c8358988364
Alex <1320538+ArtemaOK@users.noreply.github.com>
2022-10-03 00:38:39 -0300

    modified:   probabilitesDemo.js

--- probabilitesDemo.js
+++ probabilitesDemo.js
@@ -1,23 +1,29 @@
-// Demo program for showing probabilities (POSITIVE vs NEGATIVE) in a certain amount of tests.
+// Demo program for showing probabilities (POSITIVES vs NEGATIVES) in a certain amount of tests. An array called "list" will be created with the results, and then they'll be added to an object which will show the POSITIVES and NEGATIVES.
+
+// Change the number below with the amount of tests you want. 
+let testAmount = 1000;
+// Change the number below with the theorical probability (between 1 y 100) of a negative.
+let negProbability = 99;
+
 let count = 0;
 let number = 0;
 let list = [];
-let object = { POSITIVE: 0, NEGATIVE: 0 };
-// Change the number below with the amount of tests you want. 
-let testAmount = 1000;
+let object = { POSITIVES: 0, NEGATIVES: 0 };
+
 while (count < testAmount) {
     number = Math.floor(Math.random() * 100) + 1;
-    // The number below specifies the theorical probability (between 1 y 100) of a NEGATIVE.
-    if (number > 99) {
-        list.push('POSITIVE');
+    if (number > negProbability) {
+        list.push('POSITIVES');
     } else {
-        list.push('NEGATIVE');
+        list.push('NEGATIVES');
     }
     count += 1;
 }
+// This loop will add the results to the object:
 for (const result of list) {
+    // The first part of the next line specifies the KEY and the second part specifies its' VALUE:
     object[result] = object[result] += 1;
 }
 // The next lines show the amount of POSITIVEs and NEGATIVEs.
-console.log('POSITIVE:', object.POSITIVE);
-console.log('NEGATIVE:', object.NEGATIVE);
+console.log('POSITIVES:', object.POSITIVES);
+console.log('NEGATIVES:', object.NEGATIVES);
