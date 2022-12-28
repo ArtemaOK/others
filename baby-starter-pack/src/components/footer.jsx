ddbb2879b6224b913a0ce4775b3317eda00dc116
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2022-12-28 17:43:21 -0300

    modified:   baby-starter-pack/src/components/footer.jsx
    	modified:   baby-starter-pack/src/components/footer.jsx

--- baby-starter-pack/src/components/footer.jsx
+++ baby-starter-pack/src/components/footer.jsx
@@ -1,13 +1,13 @@
 function Footer(props) {
     return (
-        <div className="container-fluid footer" style={{ backgroundColor: props.bgcolor }}>
+        <footer className="container-fluid footer" style={{ backgroundColor: props.bgcolor }}>
             <div className="container-fluid d-flex justify-content-center" style={{ paddingTop: props.paddingTop }}>
                 {/* <div className="col-4"></div> */}
                 <i className="fa-brands fa-facebook footer-icons" style={{ height: props.iconHeight }}></i>
                 <i className="fa-brands fa-instagram footer-icons" style={{ height: props.iconHeight }}></i>
                 {/* <div className="col-4"></div> */}
             </div>
-        </div >
+        </footer >
     );
 }
 
